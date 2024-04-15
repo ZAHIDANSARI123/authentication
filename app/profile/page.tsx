1792445c646f1dbd5d6b5c9637f9ba98cc321fad
jahid ansari <zahidrajagp@gmail.com>
2024-04-15 16:36:40 +0530

    profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useState } from 'react'
+import axios from 'axios'
+import Link from 'next/link'
+import {toast} from 'react-hot-toast'
 
-function login () {
+
+export default function ProfilePage () {
+  const router = useRouter()
+  const [data, setData] = useState("nothing")
+
+  const getUserDetails = async () => {
+    const res = await axios.post("/api/users/me")
+    console.log(res.data);
+    setData(res.data._id)
+    
+  }
+
+  const loguot = async () => {
+    try {
+      await axios.get('/api/users/logout')
+      toast.success("logout success")
+      router.push("/login")
+    } catch (error:any) {
+      console.log(error.message);
+      toast.error(error.message)
+      
+    }
+  }
   return (
-    <div>login page</div>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <h1>Profile Page</h1>
+      <hr />
+      <h2>{data === "nothing" ? "" : <Link href=
+      {`/profile`}>{data}</Link>}</h2>
+    </div>
   )
 }
-
-export default login 
